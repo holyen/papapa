ccc2d5d214b1bdc52e10408672a11400620c6815
GaryGao <garygaochinese@gmail.com>
2018-06-12 18:08:20 +0800

    update

--- Main.py
+++ Main.py
@@ -32,10 +32,6 @@ class Main:
             os.makedirs(self.torrent_path)
 
     def get_host_ip(self):
-        """
-        查询本机ip地址
-        :return: ip
-        """
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.connect(('8.8.8.8', 80))
